297adec7ac5083da31422cb2a6ada9254328f018
Henrymarks1 <henryesmarks@gmail.com>
2022-09-02 20:52:42 -0500

    server and html

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-// const fetch = require('node-fetch');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const app = express()
-const port = 8080
+app.get('/', (req, res) => {
+  console.log("The Anonymous api created by Henry Marks and Andy Negrut")
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+io.on('connection', (socket) => {
+    console.log("user has connected")
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+      });    
+});
 
-app.get('/', (req,res) =>{
-    res.send('Welcome to the Anon API, created by Henry Marks and Andy Negrut!')
-})
-
-app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`)
-})
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
