6d196c034fdbab925d6c6c41a22cb4e95d10616b
Henrymarks1 <henryesmarks@gmail.com>
2022-09-04 18:55:46 -0500

    Doc formating

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  console.log("The Anonymous api created by Henry Marks and Andy Negrut")
-  res.sendFile('index.html', { root: "../client/" });
+app.get("/", (req, res) => {
+  console.log("The Anonymous api created by Henry Marks and Andy Negrut");
+  res.sendFile("index.html", { root: "../client/" });
 });
 
-app.get('/question', (req, res) =>{
-  res.sendFile('question.html', { root: "../client/" })
-})
+app.get("/question", (req, res) => {
+  res.sendFile("question.html", { root: "../client/" });
+});
 
-io.on('connection', (socket) => {
-    console.log(`${socket.id} has connected`)
+io.on("connection", (socket) => {
+  console.log(`${socket.id} has connected`);
 
+  //Joining a room, and alerting the client
+  socket.on("roomId", (roomId) => {
+    console.log("Joining room: ", roomId);
+    io.socketsJoin(roomId);
+    console.log(socket.rooms);
+    socket.emit("joined-room", roomId);
+  });
 
-    //Joining a room, and alerting the client
-    socket.on('roomId', (roomId) => {
-        console.log("Joining room: ", roomId)
-        io.socketsJoin(roomId)
-        console.log(socket.rooms);
-        socket.emit("joined-room", roomId)
-      });    
-
-    //Send a question to the chat
-    socket.on('question', (msg) => {
-      console.log('question: ' + msg);
-      io.emit('question', msg);
-    });  
+  //Send a question to the chat
+  socket.on("question", (msg) => {
+    console.log("question: " + msg);
+    io.emit("question", msg);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
-
