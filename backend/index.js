b5ec3bfbbf591cdf8150cbde9eededc1971144c8
Henrymarks1 <henryesmarks@gmail.com>
2022-09-04 18:53:43 -0500

    getting rooms/ question messaging

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,21 @@ app.get('/question', (req, res) =>{
 
 io.on('connection', (socket) => {
     console.log(`${socket.id} has connected`)
+
+
+    //Joining a room, and alerting the client
     socket.on('roomId', (roomId) => {
         console.log("Joining room: ", roomId)
         io.socketsJoin(roomId)
         console.log(socket.rooms);
         socket.emit("joined-room", roomId)
-        
       });    
+
+    //Send a question to the chat
+    socket.on('question', (msg) => {
+      console.log('question: ' + msg);
+      io.emit('question', msg);
+    });  
 });
 
 server.listen(3000, () => {
